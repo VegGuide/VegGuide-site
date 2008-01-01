ffd61aa6bb8b1059ae81cff10cdcdba9e0fa1953
autarch <devnull@localhost>
2008-01-01 22:08:48 +0000

    Ok, errors are being logged.

--- lib/VegGuide/Plugin/ErrorHandling.pm
+++ lib/VegGuide/Plugin/ErrorHandling.pm
@@ -18,8 +18,6 @@ sub finalize_error
 
     my @errors = @{ $self->error() || [] };
 
-    $self->log()->error($_) for @errors;
-
     my $status =
         ( grep { /unknown resource|no default/i } @errors ) ? RC_NOT_FOUND : RC_INTERNAL_SERVER_ERROR;
 
