137fde49641c8764ade8e7a0af56c9d45d94b16a
autarch <devnull@localhost>
2008-01-02 04:58:45 +0000

    actually get some useful info into the error logs

--- lib/VegGuide/Plugin/ErrorHandling.pm
+++ lib/VegGuide/Plugin/ErrorHandling.pm
@@ -4,7 +4,69 @@ use strict;
 use warnings;
 
 use HTTP::Status qw( RC_NOT_FOUND RC_INTERNAL_SERVER_ERROR );
+use VegGuide::JSON;
 
+
+# I'd really rather _not_ copy this whole thing in here, but it's the
+# only way to override how errors are logged.
+sub finalize {
+    my $self = shift;
+
+    for my $error ( @{ $self->error } ) {
+        $self->_log_error($error);
+    }
+
+    # Allow engine to handle finalize flow (for POE)
+    if ( $self->engine->can('finalize') ) {
+        $self->engine->finalize($self);
+    }
+    else {
+
+        $self->finalize_uploads;
+
+        # Error
+        if ( $#{ $self->error } >= 0 ) {
+            $self->finalize_error;
+        }
+
+        $self->finalize_headers;
+
+        # HEAD request
+        if ( $self->request->method eq 'HEAD' ) {
+            $self->response->body('');
+        }
+
+        $self->finalize_body;
+    }
+    
+    if ($self->use_stats) {        
+        my $elapsed = sprintf '%f', $self->stats->elapsed;
+        my $av = $elapsed == 0 ? '??' : sprintf '%.3f', 1 / $elapsed;
+        $self->log->info(
+            "Request took ${elapsed}s ($av/s)\n" . $self->stats->report . "\n" );        
+    }
+
+    return $self->response->status;
+}
+
+sub _log_error
+{
+    my $self  = shift;
+    my $error = shift;
+
+    # XXX - change this later to log to the apache log?
+#    if ( $error =~ /unknown resource/ )
+
+    my %error = ( uri => $self->request()->uri() . '' );
+
+    $error{user} = $self->vg_user()->real_name();
+    $error{user} .= q{ - } . $self->vg_user()->user_id()
+        if $self->vg_user()->user_id();
+
+    $error{error} = $error;
+
+    $self->log()->error( VegGuide::JSON->Encode( \%error ) );
+}
 
 sub finalize_error
 {
