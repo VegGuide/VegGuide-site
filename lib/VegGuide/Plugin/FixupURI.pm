9290f96aba2c0039a8910efecb264c9297006373
autarch <devnull@localhost>
2008-01-19 09:34:51 +0000

    Really fixup uris for apache

--- lib/VegGuide/Plugin/FixupURI.pm
+++ lib/VegGuide/Plugin/FixupURI.pm
@@ -5,22 +5,15 @@ use warnings;
 
 # A broken crawler keeps trying to fetch URIs like
 # /location/data.rss%3flocation_id=196%26include_hours=0
-# Unfortunately, Apache escapes the string _before_ mod_rewrite has a
-# chance to see it, so we have to handle it on the backend.
+# Unfortunately, Apache unescapes the string _before_ mod_rewrite has
+# a chance to see it, so we have to handle it on the backend.
 sub prepare_action
 {
     my $self = shift;
 
-    my $path = $self->request()->path();
-
-    if ( $path =~ /(.+\.rss)%3f(.+)$/ )
+    if ( $self->request()->path() =~ m{/location/[^/]+.+\.rss} )
     {
-        my $real_path = $1;
-        my $query = $2;
-
-        $query =~ s/%26/&/g;
-
-        $self->response()->redirect( q{/} . $real_path . q{?} . $query );
+        $self->response()->redirect( $self->request()->uri() );
     }
 
     return $self->NEXT::prepare_action(@_);
