4a474c597e9d352f6831606cee5b01002e98d9c5
autarch <devnull@localhost>
2008-02-19 21:48:58 +0000

    Allow loading of payment option by name

--- lib/VegGuide/Vendor.pm
+++ lib/VegGuide/Vendor.pm
@@ -3720,6 +3720,34 @@ use warnings;
 use VegGuide::Schema;
 use VegGuide::AlzaboWrapper ( table => VegGuide::Schema->Schema->PaymentOption_t );
 
+use VegGuide::Validate
+    qw( validate_with SCALAR );
+
+
+sub _new_row
+{
+    my $class = shift;
+    my %p = validate_with( params => \@_,
+                           spec =>
+                           { name => { type => SCALAR, optional => 1 },
+                           },
+                           allow_extra => 1,
+                         );
+
+    my $schema = VegGuide::Schema->Connect();
+
+    my $user;
+    if ( $p{name} )
+    {
+	my @where;
+	push @where,
+	    [ $schema->PaymentOption_t->name_c, '=', $p{name} ];
+
+	return $schema->PaymentOption_t->one_row( where => \@where );
+    }
+
+    return;
+}
 
 sub image_name
 {
