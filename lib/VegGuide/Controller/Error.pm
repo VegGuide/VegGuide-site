67456f2951806c63b3dcea820a43033a2116a85c
autarch <devnull@localhost>
2008-08-13 13:41:11 +0000

    Get rid of status setting

--- lib/VegGuide/Controller/Error.pm
+++ lib/VegGuide/Controller/Error.pm
@@ -6,16 +6,5 @@ use warnings;
 use base 'VegGuide::Controller::DirectToView';
 
 
-sub auto : Private
-{
-    my $self = shift;
-    my $c    = shift;
-
-    my $status = $c->request()->path() =~ m{/(\d+)$};
-
-    $c->response()->status($status);
-}
-
-
 1;
 
