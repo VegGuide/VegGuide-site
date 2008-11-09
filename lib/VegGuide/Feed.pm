7d96581c4f9cb7a76b8d0206825b844b3f986eed
autarch <devnull@localhost>
2008-11-09 04:19:59 +0000

    remove XML::Feed hack and require XML::Feed 0.20+

--- lib/VegGuide/Feed.pm
+++ lib/VegGuide/Feed.pm
@@ -52,37 +52,4 @@ sub _make_rss_10
     return;
 }
 
-if ( $XML::Feed::VERSION <= 0.12 )
-{
-    # This monkey patch fixes a problem where summary is empty.
-    package XML::Feed::Entry;
-
-    no warnings 'redefine';
-    *convert = sub {
-        my $entry = shift;
-        my($format) = @_;
-        my $new = __PACKAGE__->new($format);
-        for my $field (qw( title link content summary category author id issued modified )) {
-            my $val = $entry->$field();
-            next unless defined $val;
-            next if ref $val && $val->can('body') && ! defined $val->body();
-            $new->$field($val);
-        }
-        $new;
-    };
-
-    package XML::Feed::RSS;
-
-    our $PREFERRED_PARSER = "XML::RSS";
-
-    no warnings 'redefine';
-    *init_empty = sub {
-        my $feed = shift;
-        eval "use $PREFERRED_PARSER"; die $@ if $@;
-        $feed->{rss} = $PREFERRED_PARSER->new( version => '1.0' );
-        $feed->{rss}->add_module(prefix => "content", uri => 'http://purl.org/rss/1.0/modules/content/');
-        $feed;
-    };
-}
-
 1;
