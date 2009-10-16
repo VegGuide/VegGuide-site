13cacff95fce5a86614c5e732c20a4ba520da567
Dave Rolsky <autarch@urth.org>
2009-10-16 10:50:42 -0500

    Geo::Coder::Google 0.06 always gets utf8

--- lib/VegGuide/Geocoder.pm
+++ lib/VegGuide/Geocoder.pm
@@ -3,7 +3,7 @@ package VegGuide::Geocoder;
 use strict;
 use warnings;
 
-use Geo::Coder::Google;
+use Geo::Coder::Google 0.06;
 use VegGuide::Config;
 use VegGuide::Util qw( string_is_empty );
 use VegGuide::Validate qw( validate SCALAR_TYPE );
@@ -225,30 +225,6 @@ sub new
                    longitude         => $geocode_info->{Point}{coordinates}[0],
                    canonical_address => $geocode_info->{address},
                  };
-}
-
-
-{
-    package Geo::Coder::Google;
-
-    # Google does not return valid utf8 unless the user agent looks
-    # like a browser. Fucking stupid Google.
-
-    no warnings 'redefine';
-sub new {
-    my($class, %param) = @_;
-
-    my $key = delete $param{apikey}
-        or Carp::croak("Usage: new(apikey => \$apikey)");
-
-    my $v = __PACKAGE__->VERSION();
-    my $agent = 'Mozilla/5.0 (compatible; ' . __PACKAGE__ . "/$v; Google, please stop smoking crack)";
-
-    my $ua   = delete $param{ua}   || LWP::UserAgent->new(agent => $agent);
-    my $host = delete $param{host} || 'maps.google.com';
-
-    bless { key => $key, ua => $ua, host => $host }, $class;
-}
 }
 
 1;
