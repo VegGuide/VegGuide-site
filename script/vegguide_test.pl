9e4226cf368ccb4a6c80681ea2443d46d9f1b72b
Dave Rolsky <autarch@urth.org>
2009-12-07 23:17:50 -0600

    update to latest version of catalyst scripts

--- script/vegguide_test.pl
+++ script/vegguide_test.pl
@@ -1,20 +1,7 @@
 #!/usr/bin/env perl
 
-use strict;
-use warnings;
-use Getopt::Long;
-use Pod::Usage;
-use FindBin;
-use lib "$FindBin::Bin/../lib";
-use Catalyst::Test 'VegGuide';
-
-my $help = 0;
-
-GetOptions( 'help|?' => \$help );
-
-pod2usage(1) if ( $help || !$ARGV[0] );
-
-print request($ARGV[0])->content . "\n";
+use Catalyst::ScriptRunner;
+Catalyst::ScriptRunner->run('VegGuide', 'Test');
 
 1;
 
@@ -27,7 +14,7 @@ vegguide_test.pl - Catalyst Test
 vegguide_test.pl [options] uri
 
  Options:
-   -help    display this help and exits
+   --help    display this help and exits
 
  Examples:
    vegguide_test.pl http://localhost/some_action
