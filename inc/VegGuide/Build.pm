e73e5f95093f42561ca6a23d2058c689c00a3cbd
Dave Rolsky <autarch@urth.org>
2010-01-18 15:53:13 -0600

    require Crypt::SSLeay

--- inc/VegGuide/Build.pm
+++ inc/VegGuide/Build.pm
@@ -39,6 +39,8 @@ my %Requires =
       'Chart::OFC'                               => '0.07',
       'Class::AlzaboWrapper'                     => '0.14',
       'Class::Trait'                             => '0.22',
+      # For SSL-enabled openid URIs
+      'Crypt::SSLeay'                            => '0',
       'CSS::Minifier'                            => '0',
       'Data::Dump::Streamer'                     => '0',
       'Data::Pageset'                            => '0',
